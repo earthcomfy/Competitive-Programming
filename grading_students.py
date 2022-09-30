9bae367d47f7754f3ef396c8a2a74873ed18fa06
Hana Belay <66206865+earthcomfy@users.noreply.github.com>
2022-09-30 16:38:24 +0300

    Update grading_students.py

--- grading_students.py
+++ grading_students.py
@@ -1,20 +1,4 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
-
 def gradingStudents(grades):
-    # Write your code here
     new_grade = []
     
     for grade in grades:
@@ -28,21 +12,3 @@ def gradingStudents(grades):
                 new_grade.append(grade)  
     
     return new_grade
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    grades_count = int(input().strip())
-
-    grades = []
-
-    for _ in range(grades_count):
-        grades_item = int(input().strip())
-        grades.append(grades_item)
-
-    result = gradingStudents(grades)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
