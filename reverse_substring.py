ded4bb96f014063fdff2f4c66f36a86c3da9a0c4
Hana Belay <66206865+earthcomfy@users.noreply.github.com>
2022-10-02 23:05:00 +0300

    Create reverse_substring.py

---
+++ reverse_substring.py
@@ -0,0 +1,25 @@
+"""
+Reverse substring: https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+"""
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+    
+        for i in s:
+            if i != ')':
+                stack.append(i)
+
+            else:
+                substr = ''
+                
+                while stack:
+                    val = stack.pop()
+                    
+                    if val == '(':
+                        break
+                    else:
+                        substr += val[::-1]
+
+                stack.append(substr)
+        
+        return ''.join(stack)
