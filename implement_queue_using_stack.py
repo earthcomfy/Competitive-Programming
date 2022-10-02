112d270d4c3da59d4b82f6b1b68e40d5c054bf32
Hana Belay <66206865+earthcomfy@users.noreply.github.com>
2022-10-02 18:13:41 +0300

    Create implement_queue_using_stack.py

---
+++ implement_queue_using_stack.py
@@ -0,0 +1,25 @@
+"""
+Implement queue using stack: https://leetcode.com/problems/implement-queue-using-stacks/submissions/
+"""
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        
+
+    def pop(self) -> int:
+        if not self.empty():
+            return self.stack.pop(0)
+            
+
+    def peek(self) -> int:
+        if not self.empty():
+            return self.stack[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
