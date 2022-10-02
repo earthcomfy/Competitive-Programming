2fd28cb5db0e7219e21fea46902c0f9aa3e5f878
Hana Belay <66206865+earthcomfy@users.noreply.github.com>
2022-10-02 19:42:54 +0300

    Create evaluate_reverse_polish.py

---
+++ evaluate_reverse_polish.py
@@ -0,0 +1,29 @@
+"""
+Reverse Polish: https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/
+"""
+class Solution:
+    def evaluate(self, x: int, y: int, operator: str) -> int:
+        if operator == '+':
+            return x + y
+        if operator == '-':
+            return x - y
+        if operator == '*':
+            return x * y
+        return int(x * 1.0 / y)
+        
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operators = set(('+', '-', '*', '/'))
+        
+        for token in tokens:
+            if token not in operators:
+                stack.append(int(token))
+            else:
+                y = stack.pop()
+                x = stack.pop()
+                
+                res = self.evaluate(x, y, token)
+                stack.append(res)
+        
+        return stack[-1]              
+  
