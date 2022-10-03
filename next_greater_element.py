f3bebc320ad79f1c0469b6d35965e1d84b5ff47c
Hana Belay <66206865+earthcomfy@users.noreply.github.com>
2022-10-03 19:50:20 +0300

    Update next_greater_element.py

--- next_greater_element.py
+++ next_greater_element.py
@@ -3,19 +3,42 @@ Next greater element: https://leetcode.com/problems/next-greater-element-i/
 """
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]: 
+        hashmap = {}
         stack = []
+        res = []
 
-        for i in range(len(nums1)):
-            flag = False
-            for j in range(len(nums2)):
-                if nums1[i] == nums2[j]:
-                    flag = True
-                if flag and nums2[j] > nums1[i]:
-                    stack.append(nums2[j])
-                    flag = False
-                    break
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hashmap[stack.pop()] = num
+            
+            stack.append(num)
 
-            if flag:
-                stack.append(-1)
+        for num in nums1:
+            if num in hashmap:
+                res.append(hashmap[num])
+            else:
+                res.append(-1)
 
-        return stack
+        return res
+                
+                
+    
+#     Brute force solution
+#     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]: 
+#         stack = []
+
+#         for i in range(len(nums1)):
+#             flag = False
+#             for j in range(len(nums2)):
+#                 if nums1[i] == nums2[j]:
+#                     flag = True
+#                 if flag and nums2[j] > nums1[i]:
+#                     stack.append(nums2[j])
+#                     flag = False
+#                     break
+
+#             if flag:
+#                 stack.append(-1)
+
+#         return stack
+        
